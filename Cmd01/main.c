79037a08dee9b6431f6cce456bb5bd0bc50edaca
giuseppe mirabella <giuseppemirabella@MacBook-Pro-di-giuseppe.local>
2014-07-14 23:00:22 +0200

    Initial Commit

---
+++ Cmd01/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Cmd01
+//
+//  Created by giuseppe mirabella on 14/07/14.
+//  Copyright (c) 2014 Giuseppe Mirabella. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
